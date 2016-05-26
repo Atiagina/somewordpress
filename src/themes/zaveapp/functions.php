c581571e3fdaa813a0c243216631e053ee84ef69
Anna <annaatiagina@Annas-MacBook-Pro.local>
2016-05-26 16:27:18 -0500

    Changes language of recaptcha to Spanish

--- src/themes/zaveapp/functions.php
+++ src/themes/zaveapp/functions.php
@@ -124,3 +124,17 @@ $addsizes = array(
 $newsizes = array_merge($sizes, $addsizes);
 return $newsizes;
 }
+
+add_action( 'wpcf7_enqueue_scripts', 'custom_recaptcha_enqueue_scripts', 11 );
+
+function custom_recaptcha_enqueue_scripts() {
+	wp_deregister_script( 'google-recaptcha' );
+
+	$url = 'https://www.google.com/recaptcha/api.js';
+	$url = add_query_arg( array(
+		'onload' => 'recaptchaCallback',
+		'render' => 'explicit',
+	 	'hl' => 'es' ), $url );
+
+	wp_register_script( 'google-recaptcha', $url, array(), '2.0', true );
+}
